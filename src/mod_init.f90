c094e1a1ab2c8563d4b10ab4fc8b439d30380494
Somajit <dey.somajit@gmail.com>
2022-12-05 16:59:39 +0530

    FAILING WITH SEGFAULT. Save for future reference only

--- src/mod_init.f90
+++ src/mod_init.f90
@@ -1,85 +1,74 @@
 module init
+use shared
 implicit none
-
-    !TODO: radius should be derived from number of beads per cell and l0
-    double precision,parameter:: radius= 1.0d0   ! Initial cell radius
 
 contains
 
 !!! Subroutine for random initial configurations
 subroutine initial
+double precision:: radius   ! Initial/seed cell radius
+double precision:: acell,bcell,dr,mindist,dx,dy, theta1
+integer:: l,kcell,i,counter
+DOUBLE PRECISION:: rands(2)
+double precision, dimension(size(x,dim=1)) :: xcell, ycell
 
-use shared
+radius = 0.5d0*l0/dsin(pi/n) ! circumcirle of a regular n-gon with side l0
+dr = rc_rep ! initial separation between cell peripheries
+mindist = 2*radius+dr
+!box = dsqrt(m*dsqrt(3.0d0)/24.0d0) * n*l0
+! Derived from m*(area of hexagon with perimeter n*l0 or 2*pi*radius) = box^2
+box = dsqrt(m*pi*mindist*mindist/4) ! box should accommodate m circumcircles with dr separation
 
-double precision:: a,b,a1,b1,g,dr,q,theta1
-integer:: l,k1,i,t
-      DOUBLE PRECISION:: rands(2)
-      double precision, dimension(size(x,dim=1)) :: x1, y1
+x=0.d0 !DEBUG:
+y=0.d0 !DEBUG:
+xcell(1) = 0.d0
+ycell(1) = 0.d0
 
-!TODO : Box should be derived from num of cells, beads and rest spring length
-box = 46.0d0
+trial: do l=2,m
+    counter=0
 
-q = 0.5d0*dsqrt(m*7.3d0)
-dr = 0.2d0
-t = 0
-
-
-    do
-        CALL RANDOM_NUMBER(rands)
-    a = q*2.0d0*rands(1)
-    b = q*2.0d0*rands(2)
-    if((a.ge.(radius+0.05d0)).and.(b.ge.(radius+0.05d0))) exit
-    end do
-
-
-   x1(1) = a
-   y1(1) = b
-
-
-do l=2,m
-        lcell: do
-    do
-        CALL RANDOM_NUMBER(rands)
-    a1 = q*2.0d0*rands(1)
-    b1 = q*2.0d0*rands(2)
-    if((a1.ge.(radius+0.05d0)).and.(b1.ge.(radius+0.05d0))) exit
-    end do
-
-   x1(l) = a1
-   y1(l) = b1
-
-    do k1=1,l-1
-
-       g = dsqrt((x1(l)-x1(k1))**2 +(y1(l)-y1(k1))**2)
-
-       if (g.lt.(2*radius+dr)) cycle lcell
-    end do
-        exit lcell
-        end do lcell
-end do
-
-       theta1 = 0.0d0
-       
-  
-     do l=1,m  
-       do i=1,n
-        CALL RANDOM_NUMBER(rands)
+    lcell: do
+        counter = counter+1 
+        if(counter > 1000) then
+            !DEBUG: if(tradius .lt. radius/2) then 
+                !DEBUG: print*, 'I am cell num: ', l 
+                !DEBUG: exit lcell 
+            !DEBUG: end if 
+            box = box*1.1d0
+            counter = 0
+        end if 
         
-           x(l,i) = radius*cos(theta1) + 0.01d0*(2.0d0*rands(1)-1.0d0) + x1(l)
-           y(l,i) = radius*sin(theta1) + 0.01d0*(2.0d0*rands(2)-1.0d0) + y1(l)
-           theta1 = theta1 + 2.0d0*pi/n
-                                                         
-       end do
+        CALL RANDOM_NUMBER(rands)
+        acell = box*rands(1)
+        bcell = box*rands(2)
+
+        do kcell=1,l-1
+            dx = acell-xcell(kcell)
+            dy = bcell-ycell(kcell)
+            dx = dx - box*nint(dx/box)
+            dy = dy - box*nint(dy/box)
+            if ((dx*dx + dy*dy).lt.(mindist*mindist)) cycle lcell
+        end do
+
+        xcell(l) = acell
+        ycell(l) = bcell
+
+        exit lcell
+    end do lcell
+
+    do i=1,n        
+        theta1 = (i-1)*2.0d0*pi/n
+        x(l,i) = radius*dcos(theta1) + xcell(l)
+        y(l,i) = radius*dsin(theta1) + ycell(l)
      end do
+end do trial
+
 
 	return
 	end subroutine initial
 
 !! Subroutine for initializaion of noise-angle
-       subroutine initial_angle           
-
-        use shared
-
+       subroutine initial_angle()
         double precision :: m_tmp,mx_tmp,my_tmp
         integer :: i,l
         DOUBLE PRECISION:: rands(2)
